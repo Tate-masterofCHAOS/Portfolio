c25cb26adf5322f6be71cf0b26b490b1df4c8c35
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-04-02 13:42:22 -0600

    eeeeeeeee

--- ui/main.py
+++ ui/main.py
@@ -8,9 +8,28 @@
 
 def main():
     print("Hello, my name is Tate Morgan and this is my project portfolio.\n")
-    project = input("Please select a project to view: \n1. Celestial Ruins Main Menu \n2. Project 2\n3. Project 3\n4. Project 4\n5. Project 5\n6. Project 6\n7. Project 7\n8. Project 8\n9. Project 9\n10. Project 10\n")
-    if project == "1":
-        print("Hello this project is strange and can not run in the terminal to open go to the creepy face on the right and scroll down and press the triangle in the runner section")
+    while True:
+        project = input("Please select a project to view: \n1. Celestial Ruins Main Menu \n2. Project 2\n3. Project 3\n4. Project 4\n5. Project 5\n6. Project 6\n7. Project 7\n8. Project 8\n9. Project 9\n10. Project 10\n")
+        if project == "1":
+            print("Hello this project is strange and can not run in the terminal \nto open go to the creepy face on the right and scroll down and press the triangle in the runner section")
+        elif project == "2":
+            print("This project is not available")
+        elif project == "3":
+            print("This project is not available")
+        elif project == "4":
+            print("This project is not available")
+        elif project == "5":
+            print("This project is not available")
+        elif project == "6":
+            print("This project is not available")
+        elif project == "7":
+            print("This project is not available")
+        elif project == "8":
+            print("This project is not available")
+        elif project == "9":
+            print("This project is not available")
+        elif project == "10":
+            print("This project is not available")
 
 
 
