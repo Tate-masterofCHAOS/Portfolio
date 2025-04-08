3c17ea11acc5d6d8a466c4b7ba59cac1371febb6
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-04-08 18:04:31 +0000

    eee

--- Portfolio/main.py
+++ Portfolio/main.py
@@ -4,7 +4,7 @@ from movie_rec.main import main as movie
 from Password_gen.main import main as password
 from trans_morse_code.main import main as morse
 from Update_Library.main import main as libarby
-
+import time
 
 
 
@@ -14,18 +14,31 @@ def main():
     while True:
         project = input("Please select a project to view: \n1. Celestial Ruins Main Menu \n2. Financial calculator\n3. Movie Reccomender\n4. Password Generator\n5. Morse Code Translator\n6. Anime Library\n7. Exit\n")
         if project == "1":
-            print("Hello this project is strange and can not run in the terminal \nto open go to the creepy face on the left and scroll down and press the triangle in the runner section\n\n\n\n\n\n")
+            print("Hello this project is strange and can not run in the terminal \nto open go to the creepy face on the left and scroll down and press the triangle in the runner section\n\n\n")
+            time.sleep(3)
         elif project == "2":
+            print("Booting up Financial Calculator")
+            time.sleep(3)
             finance()
         elif project == "3":
+            print("Booting up Movie Reccomender")
+            time.sleep(3)
             movie()
         elif project == "4":
+            print("Booting up Password Generator")
+            time.sleep(3)
             password()
         elif project == "5":
+            print("Booting up Morse Code Translator")
+            time.sleep(3)
             morse()
         elif project == "6":
+            print("Booting up Anime Library")
+            time.sleep(3)
             libarby()
         elif project == "7":
+            print("Closing Application")
+            time.sleep(3)
             quit()
         else:
             print("Please select a valid option.")
