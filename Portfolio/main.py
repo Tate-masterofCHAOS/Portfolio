771a8396e16b7e5ea6351f0c456fe94935fc47ee
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-04-22 19:26:26 +0000

    eee

--- Portfolio/main.py
+++ Portfolio/main.py
@@ -14,30 +14,26 @@ def main():
     while True:
         project = input("Please select a project to view: \n1. Celestial Ruins Main Menu \n2. Financial calculator\n3. Movie Reccomender\n4. Password Generator\n5. Morse Code Translator\n6. Battle Simulator\n7. Exit\n")
         if project == "1":
-            print("Hello this project is strange and can not run in the terminal \nto open go to the creepy face on the left and scroll down and press the triangle in the runner section\n\n\n This is a main menu for an indie game me and my friend are making this is the main menu\n\n\n")
+            print("Hello this project is strange and can not run in the terminal \nto open go to the creepy face on the left and scroll down and press the triangle in the runner section\n\n\n This is a main menu for an indie game me and my friend are making this is the main menu we are using a coding language called GML or Game Maker Language and from this assignment i learned how to use GML to make main menu's\n\n\n")
             time.sleep(3)
         elif project == "2":
             print("This is a budget allocator and tip calculator that allows one to track expenses and compund interest")
             time.sleep(3)
             finance()
         elif project == "3":
-            print("Booting up Movie Reccomender")
-            print("This is a movie reccomender that uses specified requirements to display movies for you")
+            print("This is a movie reccomender that uses specified requirements to display movies for you using a csv file and a function to search it. I learned how useful csv files can be")
             time.sleep(3)
             movie()
         elif project == "4":
-            print("Booting up Password Generator")
-            print("This is a password generator that allows you to create a password based on your requirements")
+            print("This is a password generator that allows you to create a password based on your requirements. This implements the use of the random library.")
             time.sleep(3)
             password()
         elif project == "5":
-            print("Booting up Morse Code Translator")
-            print("This is a morse code translator that allows you to translate text to morse code and vice versa")
+            print("This is a morse code translator that allows you to translate text to morse code and vice versa. To do this I used some dictionarys and stuff")
             time.sleep(3)
             morse()
         elif project == "6":
-            print("Booting up Battle System")
-            print("This is a very basic turnbased battle system that allows you to fight a monster")
+            print("This is a very basic turnbased battle system that allows you to fight a monster which uses inner and helper functions to do the battles. This taught me the advantages of using inner functions and recursions and helper functions")
             time.sleep(3)
             battle()
         elif project == "7":
